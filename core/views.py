ea880b6ee2beb375f448bf5bf45fa74a814e93ca
Ali <alialwenni05@gmail.com>
2025-09-18 09:35:37 +0300

    finish work

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
 
 from item.models import Category, Item
-
 from .forms import SignupForm
+
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -13,20 +15,19 @@ def index(request):
         'items': items,
     })
 
+
 def contact(request):
     return render(request, 'core/contact.html')
+
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-
         if form.is_valid():
             form.save()
-
-            return redirect('/login/')
+            messages.success(request, 'Account created! You can now sign in.')
+            return redirect(reverse('core:login'))
     else:
         form = SignupForm()
 
-    return render(request, 'core/signup.html', {
-        'form': form
-    })
+    return render(request, 'core/signup.html', {'form': form})
